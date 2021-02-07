be6d0844b2126c261fb44e40f46ae6feec43bf7f
Konstantin Senkevich <konstsen@gmail.com>
2021-02-07 22:05:00 +0300

    Amend

--- src/merchant.rs
+++ src/merchant.rs
@@ -2,10 +2,6 @@ use super::market;
 
 pub trait Merchant
 {
-    type Accountant: market::Accountant + 'static;
-    type Trader: market::Trader + 'static;
-    type Sniffer: market::Sniffer + 'static;
-
     fn accountant(&self) -> std::sync::Arc<dyn market::Accountant>;
     fn trader(&self) -> std::sync::Arc<dyn market::Trader>;
     fn sniffer(&self) -> std::sync::Arc<dyn market::Sniffer>;
