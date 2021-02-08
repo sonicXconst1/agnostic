c7d6ca798d7f332ed508f1b65da61ccb7d1c98d5
Konstantin Senkevich <konstsen@gmail.com>
2021-02-08 22:13:31 +0300

    Optional conversion to agnostic coin.

--- src/trading_pair.rs
+++ src/trading_pair.rs
@@ -28,5 +28,5 @@ pub trait TradingPairConverter {
     fn to_string(&self, trading_pair: TradingPair) -> String;
     fn to_pair(&self, trading_pair: TradingPair) -> Self::Pair;
     fn from_agnostic_coin(&self, coin: Coin) -> Self::Coin;
-    fn to_agnostic_coin(&self, coin: Self::Coin) -> Coin;
+    fn to_agnostic_coin(&self, coin: Self::Coin) -> Option<Coin>;
 }
